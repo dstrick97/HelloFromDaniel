bb7b02d43ad22cb425d82c4c751654417e80d728
michaeldhanks <60441733+michaeldhanks@users.noreply.github.com>
2020-01-31 13:06:23 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Daniel");
+        System.out.println("Your code stinks. Sincerely, Michael");
     }
 }

e4f2add604e052cd5e6aefacd36c001168ca8109
michaeldhanks <60441733+michaeldhanks@users.noreply.github.com>
2020-02-05 23:02:58 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,8 @@ public class Main {
         System.out.println("How to stair");
         for (int i = 0; i < 10; i++)
             System.out.println("step " + i);
+        System.out.println("Daniel, let me know if you see this, Michael");
+        System.out.println("Your code sucks.");
+        System.out.pritln("@ 2/5/2020 @ 11pm");
     }
 }
